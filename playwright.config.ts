fd8e7ea7e3e04b569660e60b12b691b09b0bbf8f
Damian Ospara <damian89m@gmail.com>
2026-01-31 13:26:01 +0100

    Add GitHub reporter for CI environments

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: "html",
+  reporter: process.env.CI ? [["html"], ["github"]] : [["html"]],
   use: {
     baseURL: process.env.BASE_URL || "https://coding-tree.github.io/playwright-master-class/",
     trace: "on-first-retry",
